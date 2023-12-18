4c4419733567115f44ae03a0a836eb46d9508bcd
ahmed saadhelmy <ahmedsaadhelmy123@hotmail.com>
2023-12-18 23:09:59 +0200

    fix error of seeding data

--- ALSAFA/Models/DataInitializer.cs
+++ ALSAFA/Models/DataInitializer.cs
@@ -14,18 +14,18 @@
                     if (category == null)
                     {
                         context.Categories.AddRange(
-                            new Category { Id=1 ,CName="Soup"}
+                            new Category { CName="Soup"}
                             );
                         context.SubCategories.AddRange(
-                            new SubCategory { Id=1 , CatID =1 ,Name ="Face Soup",Description="Soup used for face" },
-                            new SubCategory { Id=2 , CatID =1 ,Name ="Dish Soup",Description="Soup used for clean dishes" }
+                            new SubCategory {  CatID =1 ,Name ="Face Soup",Description="Soup used for face" },
+                            new SubCategory {  CatID =1 ,Name ="Dish Soup",Description="Soup used for clean dishes" }
                            
                             );
                         context.items.AddRange(
-                            new item { Id =1 , Name ="Dove " ,Count =25 ,Price=13 ,SubID=1},
-                            new item { Id =2 , Name ="Savana " ,Count =30 ,Price=15 ,SubID=1},
-                            new item { Id =3 , Name ="Brill" ,Count =15 ,Price=35 ,SubID=2},
-                            new item { Id =4 , Name ="Feba" ,Count =102 ,Price=50 ,SubID=2}
+                            new item {  Name ="Dove " ,Count =25 ,Price=13 ,SubID=1},
+                            new item { Name ="Savana " ,Count =30 ,Price=15 ,SubID=1},
+                            new item { Name ="Brill" ,Count =15 ,Price=35 ,SubID=2},
+                            new item { Name ="Feba" ,Count =102 ,Price=50 ,SubID=2}
                             );
                         context.SaveChanges();
                     }
